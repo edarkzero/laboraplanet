e553094e979eaca9bcb6737acb6684f0ece6bc01
Edgar Cardona <fabrikdev@gmail.com>
2020-04-30 17:57:28 -0500

    FIX: now Logout without session also works

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -40,9 +43,32 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        if(!Auth::check()) {
+            return redirect('/');
+        }
+        $m = User::find(Auth::user()->id);
+        $m->activo =0;
+        $m->save();
+        if (isset($_SESSION)){
+            session_destroy();
+        }
+
+        $this->guard()->logout();
+        $request->session()->invalidate();
+
+        return redirect('/');
+    }
 
 
-        public function username()
+    public function username()
         {
             return 'correo';
         }   
